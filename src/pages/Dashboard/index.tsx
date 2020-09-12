e02f627f2a87abbaa2997f51677de0b25a9374d4
Anderson0017 <andeerson0017@gmail.com>
2020-09-12 19:40:13 -0300

    feat: concluida a pagina do dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiBookOpen, FiTv } from 'react-icons/fi';
 import AvatarDefault from '../../assets/avatar_default.png';
 import LogoImage from '../../assets/logo.png';
 import IlutraImage from '../../assets/Ilustra.png';
@@ -9,12 +10,20 @@ import {
   Profile,
   Avatar,
   Name,
+  Button,
   IconPower,
   Main,
   Group,
   Logo,
   Description,
   Image,
+  Footer,
+  Text,
+  GroupButtons,
+  Connections,
+  ButtonStudy,
+  ButtonClass,
+  ContentFooter,
 } from './styles';
 
 const Dashboard: React.FC = () => {
@@ -26,7 +35,9 @@ const Dashboard: React.FC = () => {
             <Avatar src={AvatarDefault} alt="Avatar" />
             <Name>Anderson Silva</Name>
           </Profile>
-          <IconPower />
+          <Button>
+            <IconPower />
+          </Button>
         </Header>
 
         <Main>
@@ -37,6 +48,30 @@ const Dashboard: React.FC = () => {
           <Image src={IlutraImage} alt="ilutra" />
         </Main>
       </Content>
+      <Footer>
+        <ContentFooter>
+          <Text>
+            Seja bem-vindo.
+            <br />
+            <span>O que deseja fazer ?</span>
+          </Text>
+          <GroupButtons>
+            <Connections>
+              Total de 256 conexões
+              <br />
+              já realizadas
+            </Connections>
+            <ButtonStudy to="/">
+              <FiBookOpen size={30} color="#FFF" />
+              Estudar
+            </ButtonStudy>
+            <ButtonClass to="/">
+              <FiTv size={30} color="#FFF" />
+              Dar aulas
+            </ButtonClass>
+          </GroupButtons>
+        </ContentFooter>
+      </Footer>
     </Container>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 import { FiPower } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
   height: 100vh;
   width: 100vw;
   background: var(--color-primary);
+  overflow: hidden;
 `;
 
 export const Content = styled.div`
@@ -69,4 +71,91 @@ export const Description = styled.div`
 export const Image = styled.img`
   width: 60rem;
   height: 35rem;
+`;
+
+export const Footer = styled.div`
+  width: 100%;
+  background: var(--color-line-in-white);
+  height: 100%;
+`;
+
+export const ContentFooter = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+  max-width: 112rem;
+  margin: 3.2rem auto;
+  padding: 5.6rem 3rem;
+`;
+export const Text = styled.div`
+  color: var(--color-text-base);
+  font-size: 2rem;
+
+  > span {
+    color: var(--color-text-title);
+    font-weight: 500;
+  }
+`;
+export const GroupButtons = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const Connections = styled.div`
+  color: var(--color-text-complement);
+  font-size: 1.2rem;
+  margin-right: 5.3rem;
+`;
+export const ButtonStudy = styled(Link)`
+  cursor: pointer;
+  width: 25rem;
+  height: 10.4rem;
+  border-radius: 0.8rem;
+  font: 700 2rem Archivo;
+
+  margin-right: 1.6rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  transition: background-color 0.2s;
+
+  text-decoration: none;
+  color: var(--color-button-text);
+  background: var(--color-primary-lighter);
+
+  &:hover {
+    background: var(--color-primary-light);
+  }
+
+  svg {
+    margin-right: 3rem;
+  }
+`;
+export const ButtonClass = styled(Link)`
+  cursor: pointer;
+  width: 25rem;
+  height: 10.4rem;
+  border-radius: 0.8rem;
+  font: 700 2rem Archivo;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  transition: background-color 0.2s;
+
+  text-decoration: none;
+  color: var(--color-button-text);
+  background: var(--color-secundary);
+
+  &:hover {
+    background: var(--color-secundary-dark);
+  }
+
+  svg {
+    margin-right: 3rem;
+  }
 `;
